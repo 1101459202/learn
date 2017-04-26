4b7857a6ca7a7e0becdec70fef579626680502d6
1101459202 <1101459202@qq.com>
2017-04-26 20:45:49 +0800

    c and c++ test program

---
+++ visual_studio/c_program_test/c_program_test/c_program_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float fgloab_variable;
+
+int main(int argv, char *argc[])
+{
+	float flocal_variable;
+
+	int a = 2;
+
+	if (3 <= a <= 10)
+	{
+		puts("OK!");
+	}
+
+	system("pause");
+}
